d1e3e31eb097025ca145c3a85e7a302680f10ee7
Grant Avery <grantavery@icloud.com>
2018-11-26 15:49:39 -0500

    Initial setup and upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# RandomTextPicker
+## Simple C# Console app to randomly select an item from a list of items
+
+I mostly made this to compete with a couple coworkers who had made their own randomizers for picking which restaurant to eat out at on a given day. Each had used a different language, so I made mine in C#.
+
+
+## Build:
+
+Navigate to ~/RandomTextPicker/RandomTextPicker in the console app of your choice
+
+```
+$ csc Program.cs
+```
+
+## Run:
+### On Mac
+
+
+```
+$ [mono](https://www.mono-project.com/) Program.exe
+```
+
+### On PC
+
+```
+$ Program
+```
---
+++ RandomTextPicker/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Randomizer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool quit = false;
+
+            while (quit != true)
+            {
+                Console.WriteLine("Welcome to Randomizer! Please list off the options you would like to pick from, and I'll select one of them randomly. " +
+                                  "Make sure each option is separated with a comma \",\":");
+                string list = Console.ReadLine();
+                Console.WriteLine();
+
+                if (list == "quit")
+                {
+                    quit = true;
+                }
+                else if (!string.IsNullOrEmpty(list))
+                {
+                    string[] items = list.Split(',');
+
+                    Random randomizer = new Random();
+                    int rand = randomizer.Next(0, items.Length);
+
+                    int count = 0;
+                    foreach (string item in items)
+                    {
+                        string currentItem = item;
+
+                        if (rand == count)
+                        {
+                            if (currentItem.StartsWith(" "))
+                            {
+                                currentItem = currentItem.Substring(1);
+                            }
+
+                            Console.WriteLine("Here is the randomly-selected item:");
+                            Console.WriteLine(currentItem);
+                        }
+
+                        count++;
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("If you'd like to try again, hit Enter, or type \"quit\" to close the program");
+                    string end = Console.ReadLine().ToLower();
+
+                    if (end == "quit")
+                    {
+                        quit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! You need to input text for this program to work.");
+
+                }
+            }
+
+            Environment.Exit(0);
+        }
+    }
+}
